ec9a54a348a67601a37ed215fbe9e905656d0921
Kevin Wang <ayellowrubberduck@gmail.com>
2019-09-04 13:57:19 -0700

    first commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+
+const add = function(a, b){
+    return a + b
+}
+
+const subtract = function(a, b){
+    return a - b 
+}
+
+const multiply = function (a, b){
+    return a * b 
+}
+
+const divide = function(a, b){
+    return a / b
+}
+
+const operate = function(a, b, func){
+    return func(a, b)
+}
